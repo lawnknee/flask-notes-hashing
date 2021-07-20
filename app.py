cb08ef551e8d7d7c68650f5dbfc90589feab974a
Loni <kuangloni@gmail.com>
2021-07-20 12:43:39 -0700

    register works

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, redirect, render_template, request,
-from flask_bcrypt import Bcrypt
+from flask import Flask, redirect, render_template, request
+# from flask_bcrypt import Bcrypt
 from models import db, connect_db, User
 from forms import RegisterForm
 from flask_debugtoolbar import DebugToolbarExtension
@@ -7,7 +7,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql: // /flask_notes
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///flask_notes'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 # move this out later
@@ -20,33 +20,36 @@ db.create_all()
 
 @app.route("/")
 def redirect_register():
-    """Redirect to register page"""
+    """Redirect to register page."""
     return redirect("/register")
 
 
 @app.route("/register",  methods=["GET", "POST"])
 def show_register_form():
-    """ Displays register Forms with fields
+    """ Displays register form with fields:
     username, password, email, first_name, and last_name. 
-    Registers user after submit
+    Registers user after submit.
     """
 
     form = RegisterForm()
 
     if form.validate_on_submit():
-
         username = form.username.data
         password = form.password.data
         email = form.email.data
         first_name = form.first_name.data
         last_name = form.last_name.data
 
-        new_user = User(username, password, email, first_name, last_name)
+        new_user = User(username=username, 
+                        password=password, 
+                        email=email, 
+                        first_name=first_name, 
+                        last_name=last_name)
 
         db.session.add(new_user)
         db.session.commit()
 
-        redirect("/secret")
+        return redirect("/secret")
 
     else:
-        render_template("register_user.html", form=form)
+        return render_template("register_user.html", form=form)
--- forms.py
+++ forms.py
@@ -1,8 +1,24 @@
-from wtform import Stringfield
+"""Forms for Flask Notes."""
+
 from flask_wtf import FlaskForm
-
-
-
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Email, Length
 
 
 class RegisterForm(FlaskForm):
+    """New User Register Form."""
+
+    username = StringField("Username",
+                           validators=[InputRequired(), Length(max=20)])
+
+    password = PasswordField("Password",
+                           validators=[InputRequired()])
+
+    email = StringField("Email",
+                        validators=[InputRequired(), Email(), Length(max=50)])
+
+    first_name = StringField("First Name",
+                             validators=[InputRequired(), Length(max=30)])
+    
+    last_name = StringField("Last Name",
+                             validators=[InputRequired(), Length(max=30)])
